4c2e2a9d1a314f9ffb0b063a42f028c0623fde0e
mazy99 <mazy.1999@mail.ru>
2025-12-14 17:42:58 +0300

    add use_condition.py

---
+++ examples/use_condition.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from queue import Queue
+from threading import Condition, Thread
+from time import sleep
+
+cv = Condition()
+q: Queue = Queue()
+
+
+def order_processor(name: str) -> None:
+    while True:
+        with cv:
+            while q.empty():
+                cv.wait()
+            try:
+                order = q.get_nowait()
+                print(f"{name}: {order}")
+                if order == "stop":
+                    break
+            except Exception as ex:
+                raise Exception(f"Ошибка {ex}")
+            sleep(0.1)
+
+
+if __name__ == "__main__":
+    Thread(target=order_processor, args=("thread 1",)).start()
+    Thread(target=order_processor, args=("thread 2",)).start()
+    Thread(target=order_processor, args=("thread 3",)).start()
+
+    for i in range(10):
+        q.put(f"order {i}")
+
+    for _ in range(3):
+        q.put("stop")
+
+    with cv:
+        cv.notify_all()
