ec5f9258c77ce5d0c7ce538add1ee899943db831
mazy99 <mazy.1999@mail.ru>
2025-12-14 18:15:43 +0300

    add use_event.py

---
+++ examples/use_event.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from threading import Event, Thread
+
+event = Event()
+
+
+def worker(name: str) -> None:
+    event.wait()
+    print(f"Worker: {name}")
+
+
+if __name__ == "__main__":
+    event.clear()
+
+    workers = [Thread(target=worker, args=(f"wrk {i}",)) for i in range(5)]
+
+    for w in workers:
+        w.start()
+
+    print("Main thread")
+    event.set()
